2b324dfc5dfbd69635e5d484dff55f04d3dd5ccb
arfat132 <arfatbegum90@gmail.com>
2022-06-18 00:41:50 +0300

    review page edited

--- src/Pages/Dashboard/AddReviews.js
+++ src/Pages/Dashboard/AddReviews.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { startTransition, useEffect, useState } from 'react';
 import axios from "axios";
 import { toast, ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../Firebase/firebase.init';
 import { useForm } from 'react-hook-form';
+import { FaStar } from 'react-icons/fa';
+import colorNames from 'daisyui/src/colors/colorNames';
 
 
 const AddReviews = () => {
@@ -15,7 +17,6 @@ const AddReviews = () => {
 
         const reviews = {
             name: user.displayName,
-            img: user.photoURL,
             reviews: data.reviews,
             ratings: data.ratings,
         }
@@ -40,32 +41,25 @@ const AddReviews = () => {
 
     };
 
+    const colors = {
+        orange: "#d97706",
+        gray: "#a9a9a9"
+    }
+    const stars = Array(5).fill(0)
+    const [currentValue, setCurrentValue] = useState(0);
+    const [hoverValue, setHoverValue] = useState(undefined);
 
-    // const handleAddReviews = event => {
-    //     console.log(event);
-    //     event.preventDefault();
-    //     const reviews = {
-    //         name: user.displayName,
-    //         img: user.photoURL,
-    //         reviews: event.target.reviews.value,
-    //         ratings: event.target.ratings.value,
-    //     }
-    //     console.log(reviews)
-
-    //     axios.post('http://localhost:5000/reviews', reviews)
-    //         .then(response => {
-    //             console.log(response)
-    //             const { data } = response;
-    //             console.log(data.insertedId)
-    //             if (data.insertedId) {
-    //                 toast.success('Review Send Successfully');
-    //                 event.target.reset();
-    //             }
-    //         })
-
-    // };
+    const handleClickValue = (value) => {
+        setCurrentValue(value)
+    }
+    const handleMouseOver = (value) => {
+        setHoverValue(value)
+    }
+    const handleMouseLeave = (value) => {
+        setHoverValue(undefined)
+    }
     return (
-        <div className='px-12 py-12 mx-12 mt-12 shadow-md border border-gray-100'>
+        <div className='px-12 py-12 mx-12 mt-12 shadow-md border border-gray-100 w-[600px]'>
             <h1 className='mb-5 font-bold text-2xl'>Give Review</h1>
             <hr className='py-5' />
             <form onSubmit={handleSubmit(onSubmit)}>
@@ -74,35 +68,25 @@ const AddReviews = () => {
                     <input type="name" id="name" value={user?.displayName} className="mr-72 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " />
                 </div>
                 <div className="mb-6">
-                    <label htmlFor="img" className="w-80 mb-2 text-sm font-medium text-gray-900 ">Image Url</label>
-                    <input type="text" id="img" value={user?.photoURL} className="mr-72 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " />
-                </div>
-                <div className="mb-6">
                     <label htmlFor="ratings" className="block mb-2 text-sm font-medium text-gray-900 ">Ratings</label>
-                    <input
-                        type="number"
-                        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                        {...register
-                            ("ratings", {
-                                required: {
-                                    value: true,
-                                    message: 'quantity is Required'
-                                },
-                                max: {
-                                    value: 5,
-                                    message: 'Maximum rate 5'
-                                },
-                                min: {
-                                    value: 0,
-                                    message: 'Minimum rate 0'
-                                }
-                            })}
-                    />
-                    <label className="label">
-                        {errors.ratings?.type === 'required' && <span className="label-text-alt text-red-500">{errors.ratings.message}</span>}
-                        {errors.ratings?.type === 'max' && <span className="label-text-alt text-red-500">{errors?.ratings?.message}</span>}
-                        {errors.ratings?.type === 'min' && <span className="label-text-alt text-red-500">{errors?.ratings?.message}</span>}
-                    </label>
+                    <div className='flex'>
+                        {stars.map((_, index) => {
+                            return (
+                                <FaStar
+                                    key={index}
+                                    size={24}
+                                    style={{
+                                        marginRight: 10,
+                                        cursor:"pointer"
+                                    }}
+                                    color={(hoverValue || currentValue) > index? colors.orange : colors.gray}
+                                    onClick={() => handleClickValue(index + 1)}
+                                    onMouseOver={() => handleMouseOver(index + 1)}
+                                    onMouseLeave={handleMouseLeave}
+                                ></FaStar>
+                            )
+                        })}
+                    </div>
                 </div>
                 <div className="mb-6">
                     <label htmlFor="reviews" className="block mb-2 text-sm font-medium text-gray-900 ">Review</label>
@@ -114,5 +98,6 @@ const AddReviews = () => {
         </div>
     );
 };
+
 
 export default AddReviews;
